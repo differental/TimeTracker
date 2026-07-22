072a8683f8bbfdcd7e7a6b63a6309f6147b87973
Janez Rotman <janez@janez.at>
2026-07-22 22:31:56 +0200

    initialize iOS App

---
+++ TimeTracker/TimeTracker/TimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerApp.swift
+//  TimeTracker
+//
+//  Created by Janez Rotman on 22.07.26.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
