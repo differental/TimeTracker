497740dcd56cfe1572d69019a096cb7657b4cc1c
Brian Chen <enderchen004@outlook.com>
2026-07-26 09:31:58 +0200

    temp: testing

--- src/auth.rs
+++ src/auth.rs
@@ -23,7 +23,7 @@ use serde::Deserialize;
 
 use crate::constants::ACCESS_KEY;
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct AuthQueryParams {
     key: Option<String>,
 }
@@ -34,6 +34,8 @@ pub async fn auth_user(
     next: Next,
 ) -> impl IntoResponse {
     // Authentication layer, checks query param against key
+
+    println!("{:?}", params);
 
     if let Some(ref val) = params.key
         && val.trim() == *ACCESS_KEY
